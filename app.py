d5691bd0ea270e7b9462ebc761d73230224cf4eb
abhistar <jainabhinav2412@gmail.com>
2020-09-29 10:01:41 +0530

    lol!

--- app.py
+++ app.py
@@ -12,12 +12,5 @@ def index():
 
 	return render_template("index.html", smiles=smiles)
 
-
-@app.route("/result", methods=["POST"])
-def result():
-	smiles = request.form.get("smiles")
-	return render_template("layout.html", smiles=smiles)
-
-
 if __name__ == '__main__':
     app.run(debug=True)
