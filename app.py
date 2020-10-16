7e7ee50d891bd564df01ec560d68027b9c3c77a4
abhistar <jainabhinav2412@gmail.com>
2020-10-16 23:17:45 +0530

    added 3d structure capability using 3dmol.js

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+import time
 
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def index():
 	smiles = ''
 	if request.method == "POST":
 		smiles = request.form.get("smiles")
+		if smiles != '':
+			m = Chem.MolFromSmiles(smiles)
+			AllChem.EmbedMolecule(m)
+			w = Chem.SDWriter('static/molecule.sdf')
+			w.write(m)
 
 	return render_template("index.html", smiles=smiles)
 
